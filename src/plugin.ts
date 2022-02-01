a7a1203e166c36bf28d09b8bd4ba8fe11a24516b
yellowsink <cainatkinson98@gmail.com>
2022-02-01 13:13:15 +0000

    lots of work

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,36 +1,98 @@
-console.log(
-  `~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~~ Hello, World! from HLCC ~~
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
-);
-
-import { CallExpression, Expression, transform } from "@swc/core";
+import {
+  ArrowFunctionExpression,
+  BlockStatement,
+  CallExpression,
+  Expression,
+  ExpressionStatement,
+  ExprOrSpread,
+  FunctionExpression,
+  Statement,
+  transform,
+} from "@swc/core";
 import { Visitor } from "@swc/core/Visitor.js";
-import topLevelVisit from "./topLevelVisit";
+
+const MODULE_FIND_FUNC_NAMES = [
+  "hlccAll",
+  "hlccByDName",
+  "hlccByProps",
+  "hlccByPredicate",
+];
 
 class HLCC extends Visitor {
   visitCallExpression(n: CallExpression): Expression {
-    const expr = topLevelVisit(n);
-    return expr ?? n;
+    if (n.callee.type !== "Identifier" || n.callee.value !== "hlccInject")
+      return n;
+
+    if (
+      n.arguments.length !== 2 ||
+      n.arguments[0].expression.type !== "ArrayExpression" ||
+      (n.arguments[1].expression.type !== "ArrowFunctionExpression" &&
+        n.arguments[1].expression.type !== "FunctionExpression")
+    )
+      throw new Error(
+        `Error at pos ${n.span.start}: Args to hlccInject were invalid.`
+      );
+
+    const moduleFinds = n.arguments[0].expression.elements.map(
+      (m) => m?.expression
+    );
+
+    if (
+      !moduleFinds.every(
+        // ts moment bruh
+        (m): m is CallExpression => m?.type === "CallExpression" /*  &&
+          m.callee.type === "Identifier" &&
+          MODULE_FIND_FUNC_NAMES.includes(m.callee.value) */
+      )
+    )
+      throw new Error("Module find was not a call");
+
+    const func = n.arguments[1].expression;
+
+    if (func.params.length !== moduleFinds.length)
+      console.warn(
+        "NOTE: the amount of args taken by your function does not match the amount of module finds you ask for"
+      );
+
+    const statements =
+      func.body.type === "BlockStatement"
+        ? func.body.stmts
+        : [
+            <ExpressionStatement>{
+              type: "ExpressionStatement",
+              expression: func.body,
+            },
+          ];
+
+    return {
+      ...func,
+      body: {
+        type: "BlockStatement",
+        span: func.span,
+        stmts: statements
+      }
+    }
   }
 }
 
-await transform(
+const transformed = await transform(
   `
 
 console.log((() => "unimportant")())
 
-hlccInject(() => {
-  const mods = hlccModules();
-  const SettingsView = hlccModByDName("SettingsView");
-  const { getChannel } = hlccModByProps("getChannel", "getCategory");
+hlccInject([
+    hlccAll(),
+    hlccByDName("SettingsView"),
+    hlccByProps("getChannel", "getCategory")
+  ],
+  (mods, SettingsView, { getChannel }) => {
   console.log(SettingsView, getChannel, mods.length);
 });
-
-hlccInject(() => hlccModules())
 
 `,
   {
     plugin: (m) => new HLCC().visitProgram(m),
   }
 );
+
+console.log(transformed.code)
